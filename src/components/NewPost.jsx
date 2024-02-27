f10036cbc0d6647f6df2590c6ee0e5942a1b778e
Kilian Kielman <kilian@fastitsolutions.nl>
2024-02-27 21:26:19 +0100

    updated app

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -2,27 +2,45 @@ import { useState } from 'react';
 
 import classes from './NewPost.module.css';
 
-function NewPost() {
+function NewPost({ onCancel, onAddPost }) {
   const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
 
-  function changeBodyHandler(event) {
+  function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
   }
 
+  function autherChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const postData = {
+      body: enteredBody,
+      author: enteredAuthor,
+    };
+    console.log(postData);
+    onAddPost(postData);
+    onCancel();
+  }
+
   return (
-    <form className={classes.form}>
-      {' '}
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
-        {' '}
-        <label htmlFor="body">Text</label>{' '}
-        <textarea id="body" required rows={3} onChange={changeBodyHandler} />{' '}
-      </p>{' '}
-      <p>{enteredBody}</p>
+        <label htmlFor="body">Text</label>
+        <textarea id="body" required rows={3} onChange={bodyChangeHandler} />
+      </p>
       <p>
-        {' '}
-        <label htmlFor="name">Your name</label>{' '}
-        <input type="text" id="name" required />{' '}
-      </p>{' '}
+        <label htmlFor="name">Your name</label>
+        <input type="text" id="name" required onChange={autherChangeHandler} />
+      </p>
+      <p className={classes.actions}>
+        <button type="button" onClick={onCancel}>
+          Cancel
+        </button>
+        <button>Submit</button>
+      </p>
     </form>
   );
 }
